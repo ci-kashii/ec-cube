8edaf60ce1380036f803945bfcf8791b7aa4dbf6
ci-wataru-kashii <ptn.kashii-itm@colony-i.com>
2024-09-30 10:58:44 +0900

    fix UpdateSchemaDoctrineCommand

--- src/Eccube/Command/UpdateSchemaDoctrineCommand.php
+++ src/Eccube/Command/UpdateSchemaDoctrineCommand.php
@@ -62,7 +62,7 @@ class UpdateSchemaDoctrineCommand extends BaseUpdateSchemaDoctrineCommand
     /**
      * {@inheritDoc}
      */
-    protected function configure(): void: void
+    protected function configure(): void
     {
         parent::configure();
 
